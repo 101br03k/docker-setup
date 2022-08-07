4b01d5b816d285a8e8c83b6c3f606c9b1df9149d
A3 <59868436+101br03k@users.noreply.github.com>
2022-08-07 10:07:09 +0200

    Create run-docker-compose.sh

---
+++ run-docker-compose.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo"what service do you want to run"
+echo"1) docker"
+echo"2) portainer"
+echo"3) dash."
+echo"4) watchtower"
+echo"please type the number of the service"
+read bash $service
+
+if if [ $service = "1" ]
+then
+<commands>
+fi
