414edb72907f153ac9ea705e4b3b792ef15e3d3c
A3 <59868436+101br03k@users.noreply.github.com>
2022-08-07 11:31:55 +0200

    Delete speedtest-tracker.sh

--- speedtest-tracker.sh
+++
@@ -1,7 +0,0 @@
-docker create \
-      --name=speedtest \
-      -p 8765:80 \
-      -v /path/to/data:/config \
-      -e OOKLA_EULA_GDPR=true \
-      --restart unless-stopped \
-      henrywhitaker3/speedtest-tracker
